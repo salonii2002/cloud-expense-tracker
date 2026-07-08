4ee04ec7d1216afb72ba622bb0739e0cb145e597
saloni2002 <salonibhosale521@gmail.com>
2026-07-08 22:07:35 +0530

    Initial commit

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../frontend")));
+app.use(express.static(path.join(__dirname, "frontend")));
 
 // In-memory expense storage
 let expenses = [];
@@ -14,7 +14,7 @@ let expenses = [];
 
 // Home Page
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
 // Health Check API
